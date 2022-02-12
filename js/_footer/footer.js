3937caf7abc1f74d726b85c486e2e04b0f4d51fe
Venkateshwaran Selvaraj <venkateshwaran.selvaraj@gmail.com>
2022-02-12 16:14:08 +0530

    Added Landing page for the ecommerce site

---
+++ js/_footer/footer.js
@@ -0,0 +1,16 @@
+const footerTemplate = document.createElement('template');
+footerTemplate.innerHTML = `<footer class="feather mb-8 mt-4 flex flex-justify-center flex-items-center px-8 text-center">
+  <div>
+    <p class="text-lg mt-2 text-gray-600"> All images used are solely for non-profit educational purpose.</p>
+  </div>
+</footer>`;
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = footerTemplate.innerHTML;
+  }
+}
+customElements.define('footer-component', Footer);
